f308646c75686e3465f4aa90011c60a4eebbdace
KoboldM2 <mikoi.lukban@gmail.com>
2020-07-07 02:13:31 +0800

    Working Version - v0.1

---
+++ src/components/MainContent.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import Stuff from "./Stuff";
+import Contact from "./Contact";
+
+function MainContent() {
+  return (
+    <HashRouter>
+      <div class = "content">
+        <h1>Simple SPA</h1>
+        <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/stuff">Stuff</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+        </ul>
+      </div>
+
+      <div className="content">
+        <Route exact path="/" component={Home}/>
+        <Route path="/stuff" component={Stuff}/>
+        <Route path="/contact" component={Contact}/>
+      </div>
+    </HashRouter>  
+  );
+  }
+  
+  export default MainContent;
+  
